3811f25c9e32e4a3d2974c9b9f7ea9d3e6ca7fbf
Andressa <andressa.sena@mutua.com.br>
2024-01-24 17:27:41 -0300

    Front-End do Curso

---
+++ exercicio.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">   
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,700;0,900;1,300;1,700;1,900&family=Silkscreen&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/estilo.css">
+    <link rel="stylesheet" href="assets/css/exercicio.css">
+    <title>Exercício</title>
+</head>
+<body class="exercicio">
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização dos Exercícios</h2>
+    </header>
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+    <main class="principal">
+        <div class="conteudo">
+            
+        </div>
+    </main>
+    <footer class="rodape">
+        COD3R e ANDRESSA MONTEIRO &copy <?= date('Y') ?>
+    </footer>
+</body>
+</html>
--- index.php
+++ index.php
@@ -19,7 +19,7 @@
                 <div class="modulo verde">
                     <h3>Módulo 01</h3>
                     <ul>
-                        <li><a href="#">Exercício A</a></li>
+                        <li><a href="exercicio.php">Exercício A</a></li>
                         <li><a href="#">Exercício B</a></li>
                         <li><a href="#">Exercício C</a></li>
                     </ul>
@@ -92,7 +92,7 @@
         </div>
     </main>
     <footer class="rodape">
-        COD3R e ANDRESSA &copy 2024
+        COD3R e ANDRESSA MONTEIRO &copy <?= date('Y') ?>
     </footer>
 </body>
 </html>
