2caabd52342860cb8a39406cf44d4232773452fe
Andressa <andressa.sena@mutua.com.br>
2024-01-26 16:29:18 -0300

    Melhorando Páginas

--- tipos/int.php
+++ tipos/int.php
@@ -4,11 +4,9 @@
 echo 11;
 echo '<br>';
 
-var_dump(11);
-echo '<br>';
-
 echo PHP_INT_MAX, '<br>';
 echo PHP_INT_MIN, '<br>';
-echo 017, '<br>'; // Base octal
+echo 017, '<br>'; // Base octadecimal
 echo 0b11000111001, '<br>'; // Base binária
-echo 0x117acf0; // Base hexa
+echo 0b011, '<br>'; // Base binária
+echo 0x117acf0, '<br>'; // Base hexadecimal
