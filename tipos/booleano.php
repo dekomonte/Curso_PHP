bf3874d434d98466fbe6f42c13e090a344d88854
Andressa <andressa.sena@mutua.com.br>
2024-01-29 14:31:50 -0300

    Terminando Módulo de Tipos

--- tipos/booleano.php
+++ tipos/booleano.php
@@ -1,29 +1,50 @@
 <div class="titulo">Tipo Booleano</div>
 
 <?php
-echo 'True:';
+echo "<p>True é convertido pra 1 no browser: </p>";
+echo 'True: ';
 echo TRUE;
-echo 'False:';
+echo '<br>';
+echo 'False: ';
 echo FALSE;
+echo '<br>';
+echo '<br>';
 
-echo '<br> 1 ' . var_dump(true);
-echo '<br> 2 ' . var_dump(TRUE);
-echo '<br> 3 ' . var_dump(False);
-echo '<br> 4 ' . var_dump('false');
-echo '<br> 5 ' . is_bool(false);
-echo '<br> 6 ' . is_bool('true');
+echo '1 ';
+var_dump(true);
+echo '2 ';
+var_dump(TRUE);
+echo '3 ';
+var_dump(False);
+echo '4 ';
+var_dump('false');
+echo '5 ';
+echo is_bool(false);
+echo '<br>';
+echo '6 ';
+echo is_bool('true');
 
-// Fazer as regras de conversões
-echo '<p>Regras:</p>';
-echo '<br>' . var_dump((bool) 0); // apenas zero é false
-echo '<br>' . var_dump((bool) 20);
-echo '<br>' . var_dump((bool) -1);
-echo '<br>' . var_dump((bool) 0.0);
-echo '<br>' . var_dump((bool) 0.0000000001);
-echo '<br>' . var_dump((bool) ""); // false
-echo '<br>' . var_dump((bool) "0"); // false
-echo '<br>' . var_dump((bool) " "); // todo resto é true
-echo '<br>' . var_dump((bool) "00");
-echo '<br>' . var_dump((bool) "false");
 
-echo '<br>' . var_dump(!!"false");
+echo '<p>Regras de Conversão:</p>';
+echo 'a) ';
+var_dump((bool) 0); //Apenas zero é false
+echo 'b) ';
+var_dump((bool) 20); //True
+echo 'c) ';
+var_dump((bool) -1); //True
+echo 'd) ';
+var_dump((bool) 0.0); //False
+echo 'e) ';
+var_dump((bool) 0.0000000001); //True
+echo 'f) ';
+var_dump((bool) ""); //False
+echo 'g) ';
+var_dump((bool) "0"); //False
+echo 'h) ';
+var_dump((bool) " "); //Todo resto é true
+echo 'i) ';
+var_dump((bool) "00"); //True
+echo 'j) ';
+var_dump((bool) "false"); //True
+echo 'k) ';
+var_dump(!!"false"); //True
--- tipos/conversoes.php
+++ tipos/conversoes.php
@@ -1,46 +1,41 @@
 <div class="titulo">Conversões</div>
 
 <?php
+echo (PHP_INT_MAX);
+echo '<br>';
 echo is_int(PHP_INT_MAX);
-
-// int para float
 echo '<br>';
+
+echo '<p>Int para Float</p>';
 var_dump(PHP_INT_MAX + 1);
-echo '<br>';
+//echo '<br>';
 var_dump(1 + 1.0);
-echo '<br>';
+//echo '<br>';
 var_dump((float) 3);
+echo '<br>';
 
-// float para int
-echo '<p>Float para int</p>';
+
+echo '<p>Float para Int</p>';
 var_dump((int) 6.8);
-echo '<br>';
-var_dump(intval(2.999));
-echo '<br>';
+//echo '<br>';
+var_dump(intval(2.999)); //Pega apenas o valor inteiro
+//echo '<br>';
 var_dump((int) round(2.8));
+echo '<br>';
 
-// operações com string
-echo '<p>Strings</p>';
+
+echo '<p>Operações com Strings</p>';
 var_dump(3 + "2");
-echo '<br>';
+//echo '<br>';
 var_dump("3" + 2);
-echo '<br>';
+//echo '<br>';
 var_dump("3" . 2);
-echo '<br>', is_string("3" . 2);
-// echo '<br>', is_string("3" + 2);
-echo '<br>';
-var_dump(1 + "cinco");
-echo '<br>';
-var_dump(1 + "5 cinco");
-echo '<br>';
-var_dump(1 + "cinco 5");
-echo '<br>';
-var_dump(1 + "2+5");
-echo '<br>';
+//echo '<br>';
 var_dump(1 + "3.2");
-echo '<br>';
+//echo '<br>';
 var_dump(1 + "-3.2e2");
-echo '<br>';
+//echo '<br>';
 var_dump((int) "10.5");
-echo '<br>';
+//echo '<br>';
 var_dump((float) "10.5");
+echo '<br>';
