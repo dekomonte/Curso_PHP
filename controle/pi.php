1b3969be2ed515efc631489a0c49f52f180a7e67
Andressa <andressa.sena@mutua.com.br>
2024-01-31 16:01:18 -0300

    Adicionando Módulos

--- controle/pi.php
+++ controle/pi.php
@@ -6,4 +6,18 @@ echo pi();
 $pi = 3.14;
 
 echo '<p class="divisao">Exemplo</p>';
+echo 'Tarefa: comparar valores de PI e decidir sobre sua tolerância.<br>';
+
+$tol = 0.0001;
+$dif = abs($pi-pi());
+
+if ($dif <= $tol){
+    echo "Correto<br>";
+}
+else{
+    echo "INCORRETO<br>";
+}
+
+
+
 
