e5b2950486255d5c9c14e731c21aacbd7973471e
Andressa <andressa.sena@mutua.com.br>
2024-02-05 15:32:47 -0300

    Atualizando Alguns Tópicos

--- controle/desafio_operadores_logicos.php
+++ controle/desafio_operadores_logicos.php
@@ -37,7 +37,37 @@ echo $resumo;
 </style>
 
 <?php
-echo $_POST['t1'];
-echo $_POST['t2'];
 
+if(isset($_POST['t1']) && isset($_POST['t2'])) {
+    $t1 = $_POST['t1'] === '1';
+    $t2 = !!$_POST['t2'];
+    $tv = '';
+    $sorvete = false;
+
+    if($t1 && $t2) {
+        $tv = '50"';
+    } elseif($t1 xor $t2) {
+        $tv = '32"';
+    }
+
+    if($t1 or $t2) {
+        $sorvete = true;
+    }
+
+    if($tv) {
+    $resultado = "Vamos comprar uma TV de $tv polegadas!";
+    } else {
+        $resultado = "Sem TV dessa vez :(";
+    }
+
+    $saudavel = !$sorvete;
+
+    if($saudavel) {
+        $resultado .= 'Estamos mais saudáveis.';
+    } else {
+        $resultado .= ' Sorvete liberado \o/!';
+    }
+
+    echo "<p>$resultado</p>";
+}
 ?>
