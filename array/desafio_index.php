d7e1d0d6ddf077f5c54c4c991af56d03de9dc029
Andressa <andressa.sena@mutua.com.br>
2024-02-15 16:29:25 -0300

    Adição de Desafios

--- array/desafio_index.php
+++ array/desafio_index.php
@@ -1,5 +1,26 @@
 <div class="titulo">Desafio Index</div>
 
+<p>
+    Não faça isso! NUNCA!
+</p>
+
+<pre>
+    <code class="code-style">
+        $lista = array(
+            1,
+            4 =>  2,
+            3 =>  3,
+            'a' =>  4,
+            5,
+            '9' =>  6,
+            '06' => 7,
+            0 => 8
+        );
+
+        print_r($lista);
+    </code>
+</pre>
+
 <?php
 $lista = array(
     1,
@@ -12,6 +33,6 @@ $lista = array(
     0 => 8
 );
 
-// Array([0] => 8 [4] => 2 [3] => 3 [a] => 4 [5] => 5
-// [9] => 6 => [06] => 7)
+// Array([0] => 8 [4] => 2 [3] => 3 [a] => 4 [5] => 5 [9] => 6 => [06] => 7)
 print_r($lista);
+var_dump($lista);
--- array/mapa.php
+++ array/mapa.php
@@ -6,15 +6,8 @@ $dados = array(
     "cor" => "verde",
     "peso" => 49.8
 );
-
+echo '<p>Chave-Valor</p>';
 print_r($dados);
-
-var_dump($dados[0]);
-echo '<br>' . $dados["idade"];
-echo '<br>' . $dados["cor"];
-echo '<br>' . $dados["peso"];
-echo '<br>';
-var_dump($dados["outra_informacao"]);
 
 $lista = array(
     "a",
@@ -37,3 +30,62 @@ print_r($lista);
 $lista['vinte'] = 'j';
 echo '<br>';
 print_r($lista);
+?>
+
+<p>
+Array Associativo: Array no PHP é um mapa ordenado (valor e chave).
+</p>
+
+<pre>
+    <code class="code-style">
+        $conta1 = [
+            'titular'=>'Andressa',
+            'saldo'=>20
+        ];
+        $conta2 = [
+            'titular'=>'Fu',
+            'saldo'=>14900
+        ];
+        $conta3 = [
+            'titular'=>'Ayo',
+            'saldo'=>5600
+        ];
+
+        $listaContas = [$conta1,$conta2,$conta3];
+
+        #Loop - Método 1 (for)
+        for($i=0;$i<=sizeof($listaContas);$i++){
+            echo $listaContas[$i]['titular'];
+            echo $listaContas[$i]['saldo'];
+        }
+    </code>
+</pre>
+
+<?php
+//Array Associativo
+#Array no PHP é um mapa ordenado (valor e chave)
+$conta1 = [
+    'titular'=>'Andressa',
+    'saldo'=>20
+];
+$conta2 = [
+    'titular'=>'Fu',
+    'saldo'=>14900
+];
+$conta3 = [
+    'titular'=>'Ayo',
+    'saldo'=>5600
+];
+
+$listaContas = [$conta1,$conta2,$conta3];
+echo '<br>';
+
+#Loop - Método 1 (for)
+for($i=0;$i<sizeof($listaContas);$i++){
+    echo $listaContas[$i]['titular'].PHP_EOL;
+    echo $listaContas[$i]['saldo'].PHP_EOL;
+    echo '<br>';
+}
+
+echo '<br>';
+?>
