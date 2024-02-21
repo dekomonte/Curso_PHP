1c427c301e8a7e7a89ab68e7ff2c9e2d0fbd941c
Andressa <andressa.sena@mutua.com.br>
2024-02-21 15:54:01 -0300

    Mudando Detalhes

--- poo/conceito.php
+++ poo/conceito.php
@@ -1,3 +1,13 @@
 <div class="titulo">Conceitos</div>
 
-<p>Classe -> Atributos e Comportamentos.</p>
+<h2>Classe</h2>
+<p>Classe -> Atributos(Dados) e Comportamentos(Métodos/Funções)</p>
+<p>A classe define um tipo personalizado - Estrutura de Dados</p>
+<p>Uma classe representa uma abstração(simplificação)</p>
+
+<h2>Objeto</h2>
+<p>Objeto é sinônimo de instância</p>
+
+<h2>Classe x Objeto</h2>
+<p>Classe é o molde</p>
+<p>O Objeto é o item criado a partir do molde</p>
