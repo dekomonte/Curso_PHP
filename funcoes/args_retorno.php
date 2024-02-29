3ce2424a977e44ab0c7969e3b2de8151b723ca88
Andressa <andressa.sena@mutua.com.br>
2024-02-29 17:35:11 -0300

    Mudando Itens

--- funcoes/args_retorno.php
+++ funcoes/args_retorno.php
@@ -2,43 +2,72 @@
 
 <?php
 function obterMensagem() {
-    return 'Seja bem vindo(a)!';
+    return 'Olá, sou uma função!';
 }
-
-obterMensagem();
-$m = obterMensagem();
-echo $m;
-echo '<br>', obterMensagem();
-echo '<br>';
-var_dump(obterMensagem());
 
 function obterMensagemComNome($nome) {
-    return "Bem vindo, {$nome}!";
+    return "Olá, {$nome}!";
 }
-
-echo '<br>', obterMensagemComNome('Wagner');
-echo '<br>', obterMensagemComNome('Tiago');
-
-function soma($a, $b) {
-    return $a + $b;
-}
-
-$x = 4;
-$y = 5;
-echo '<br>', soma(45, 78);
-echo '<br>', soma($x, $y);
 
 function trocarValor($a, $novoValor) {
     $a = $novoValor;
 }
 
-$variavel = 1;
-trocarValor($variavel, 3);
-echo '<br>', $variavel;
-
 function trocarValorDeVerdade(&$a, $novoValor) {
     $a = $novoValor;
 }
 
+function soma($a, $b) {
+    return $a + $b;
+}
+
+//--------------------------
+echo "<p class=divisao> Função Obter Mensagem</p>";
+obterMensagem();
+$m = obterMensagem();
+echo $m;
+echo '<br>';
+echo obterMensagem();
+echo '<br>';
+var_dump(obterMensagem());
+
+//--------------------------
+echo "<p class=divisao> Função Obter Mensagem com Nome</p>";
+echo obterMensagemComNome('Wagner');
+echo '<br>';
+echo obterMensagemComNome('Tiago');
+echo '<br>';
+echo obterMensagemComNome('Ayobami');
+echo '<br>';
+echo obterMensagemComNome('Pedro');
+echo '<br>';
+echo obterMensagemComNome('Korn');
+echo '<br>';
+echo obterMensagemComNome('Gabriel');
+echo '<br>';
+
+//--------------------------
+echo "<p class=divisao> Função Soma</p>";
+$x = 23;
+$y = 58;
+echo soma(45, 78);
+echo '<br>';
+echo soma($x, $y);
+echo '<br>';
+echo soma($x, 214);
+echo '<br>';
+echo soma(756, $y);
+echo '<br>';
+
+//--------------------------
+echo "<p class=divisao> Função Troca Valor</p>";
+echo "Mostra a diferença entre argumentos como referência e valor.","<br>";
+$variavel = 1;
+trocarValor($variavel, 3);
+echo $variavel;
+echo '<br>';
+
+//--------------------------
+echo "<p class=divisao> Função Troca Valor de Verdade</p>";
 trocarValorDeVerdade($variavel, 5000);
-echo '<br>', $variavel;
+echo $variavel;
